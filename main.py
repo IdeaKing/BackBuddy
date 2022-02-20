099ebec1a837c544a2c3382af7d439f26c8c4336
Thomas Chia <66394854+IdeaKing@users.noreply.github.com>
2022-02-19 19:48:47 -0500

    tf_pose integration

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
-import pyforms
+import os
+
+from zipfile import ZipFile
+
+import cv2
+import gdown
+import numpy as np
 import tensorflow as tf
 
-from src.GUI.player import ComputerVisionAlgorithm
+from tf_pose.estimator import TfPoseEstimator
+from src.gui.gui import gui_args
 
-# Global parameters
-IMAGE_SHAPE = (256, 256)
-IMAGE_CHANNELS = 3
-
-"""
-KEYPOINTS_MODEL_PATH = "models/keypoints"
-CLASSIFIER_MODEL_PATH = "models/classifier"
-
-KEYPOINTS_MODEL = tf.keras.models.load_model(
-    KEYPOINTS_MODEL_PATH
-)
-CLASSIFIER_MODEL = tf.keras.models.load_model(
-    CLASSIFIER_MODEL_PATH
-)
-"""
-GUI = ComputerVisionAlgorithm(
-    keypoints_model=KEYPOINTS_MODEL,
-    classifier_model=CLASSIFIER_MODEL
-)
 if __name__ == "__main__":
-    pyforms.start_app(GUI)
+    # Global parameters
+    IMAGE_SHAPE = (256, 256)
+    IMAGE_CHANNELS = 3
+    KEYPOINTS_MODEL_PATH = "tf_pose/models/keypoints/graph_opt.pb"
+    
+    # Download the models if not downloaded
+    # Keypoints
+    """
+    if os.path.isfile(KEYPOINTS_MODEL_PATH + ".zip") == False:
+        gdown.download(
+            url = "https://drive.google.com/uc?id=1CnzHB1r6xFAZSc1a0vL-IKkZN6hpFH63",
+            output = KEYPOINTS_MODEL_PATH + ".zip"
+        )
+        # Unzip the files
+        with ZipFile(KEYPOINTS_MODEL_PATH + ".zip", "r") as unzip:
+            # Extract all the contents of zip file in current directory
+            unzip.extractall("models")
+    """
+    e = TfPoseEstimator(
+        KEYPOINTS_MODEL_PATH, 
+        target_size=IMAGE_SHAPE)
+    
+    # Start the GUI
+    args = gui_args()
+
+    """
+    print("Camera Number: ", args.camera_number)
+    print("Camera On: ", args.camera_on)
+
+    if args.camera_on == True:
+        cap = cv2.VideoCapture(0) #args.camera_number)
+        while True:
+            ret, frame = cap.read()
+            frame = cv2.resize(
+                frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA
+            )
+            cv2.imshow('Input', frame)
+            humans = e.inference(
+                frame, 
+                resize_to_default=(IMAGE_SHAPE[0] > 0 and IMAGE_SHAPE[1] > 0), 
+                upsample_size=4
+            )
+            print(humans)   
+    """         
---
+++ src/GUI/gui.py
@@ -0,0 +1,38 @@
+from gooey import Gooey, GooeyParser
+import cv2
+
+@Gooey(    
+    dump_build_config = False,
+    auto_start=False,
+    default_size=(1000, 1000),
+    program_name = "BackBuddies Trainer Analysis Program",)
+    #image_dir="")
+def gui_args():
+    parser = GooeyParser(
+        description = "Your Personal Trainer")
+    parser_group = parser.add_argument_group(
+        "Select files")
+    parser_group.add_argument(
+        "--Video-File",
+         "-v",
+         help = "Path to the input file with video.", 
+         widget = "FileChooser")
+    """
+    parser_group = parser.add_argument_group(
+        "Select Camera if Needed")
+    parser_group.add_argument(
+        "--Camera-On",
+        "-o",
+        help = "Select Camera (for live feed)",
+        widget = "BlockCheckbox")
+    parser_group.add_argument(
+        "--Camera-Number",
+        "-n",
+        help = "Select Camera (for live feed)",
+        widget = "IntegerField")
+    """
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    gui_args()
